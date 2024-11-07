d1dc4f78a285ae4c7608dcaa4cc2f82c07c60c5e
Johnathan Sales <johnathansales08@gmail.com>
2024-11-06 22:58:59 -0300

    Projeto Editado e Finalizado
    Adicionei pois o anterior estava com falhas nas versões mobile e tablet

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    "Educação Financeira", 
+    "O que é um fundo de emergência?", 
+    "É uma reserva financeira para cobrir despesas imprevistas."
+)
+
+criaCartao(
+    "Educação Financeira", 
+    "O que são investimentos de baixo risco?", 
+    "São investimentos que oferecem uma menor probabilidade de perda, como a poupança ou os títulos públicos."
+)
+
+criaCartao(
+    "Bolsa de Valores", 
+    "O que é análise técnica?", 
+    "É o estudo dos preços passados de ações para prever seus futuros movimentos no mercado."
+)
+
+criaCartao(
+    "Bolsa de Valores", 
+    "O que significa IPO?", 
+    "IPO (Initial Public Offering) é a oferta pública inicial de ações de uma empresa para o mercado."
+)
+
+criaCartao(
+    "Bolsa de Valores", 
+    "O que são dividendos?", 
+    "Dividendos são a parte do lucro de uma empresa distribuída entre seus acionistas."
+)
+
+criaCartao(
+    "Economia", 
+    "O que é um monopólio?", 
+    "É uma situação onde uma única empresa domina todo o mercado de um produto ou serviço."
+)
+
+criaCartao(
+    "Empreendedorismo", 
+    "O que é escalabilidade em um negócio?", 
+    "É a capacidade de um negócio crescer sem aumentar proporcionalmente os custos."
+)
+
+criaCartao(
+    "Empreendedorismo", 
+    "O que é um pitch de negócios?", 
+    "É uma apresentação curta e persuasiva sobre uma ideia ou projeto para investidores."
+)
+
+criaCartao(
+    "Investimentos", 
+    "O que são fundos de investimento?", 
+    "São aplicações coletivas, onde investidores aplicam recursos em conjunto para diversificar o portfólio."
+)
+
+criaCartao(
+    "Investimentos (Bônus)", 
+    "O que é um investimento de alto risco?", 
+    "São investimentos que oferecem maior potencial de retorno, mas também possuem maior probabilidade de perdas, como ações e criptomoedas."
+)
